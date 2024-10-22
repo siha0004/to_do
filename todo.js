2dc639b0d930f7a718c349484ff5696e1717b85f
Marie Wulff <mawu0001@stud.kea.dk>
2024-10-22 14:05:06 +0200

    event bubleling  started + listener på checkbox, og ændringer started

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@ const activeTasks = document.querySelector("#list_active");
 const doneTasks = document.querySelector("#list_done");
 const inputTask = document.querySelector("#tasklist");
 const button = document.querySelector("button");
+let container;
 
 //Model
 let list;
@@ -20,9 +21,17 @@ init();
 
 function init() {
   button.addEventListener("click", createClick);
+  //container.addEventListener("click", handleClick);
 
   updateView();
 }
+
+// function handleClick(event) {
+//   if (event.target) {
+//   }
+//   console.log(event.target);
+//   console.log(event.currentTarget);
+// }
 
 function addTaskToList(task) {
   list.push(task);
@@ -36,14 +45,14 @@ function changeDoneToList(task) {
   list.push(task);
 }
 
-function isChecked(event) {
-  event.preventDefault();
-  if (this.checked) {
-    console.log("checked");
-  } else {
-    console.log("not checked");
-  }
-}
+// function isChecked(event) {
+//   event.preventDefault();
+//   if (this.checked) {
+//     console.log("checked");
+//   } else {
+//     console.log("not checked");
+//   }
+// }
 
 function updateView() {
   // Tømmer listerne
@@ -53,22 +62,44 @@ function updateView() {
   list.forEach((each) => {
     if (each.done === true) {
       doneTasks.innerHTML += `<li data-id="${each.id}"><input type="checkbox" name="done" id="done" class="checkbox"> ${each.description} <input type="number" name="" id="">
- <button  class="trash">slet</button></li>`;
+ <button class="trash">slet</button></li>`;
     } else {
       activeTasks.innerHTML += `<li data-id="${each.id}"><input type="checkbox" name="done" id="done" class="checkbox"> ${each.description} <input type="number" name="" id="">
-  <button  class="trash">slet</button></li>`;
+  <button class="trash">slet</button></li>`;
     }
   });
-
-  document.querySelectorAll(".trash").forEach((each) => {
-    each.addEventListener("click", trashClick);
+  document.querySelectorAll("li").forEach((elm) => {
+    elm.addEventListener("click", liKlik);
   });
+  // document.querySelectorAll(".trash").forEach((each) => {
+  //   each.addEventListener("click", trashClick);
+  // });
 
-  document.querySelectorAll(".checkbox").forEach((each) => {
-    each.addEventListener("click", isChecked);
-  });
+  // document.querySelectorAll(".checkbox").forEach((each) => {
+  //   each.addEventListener("click", isChecked);
+  // });
 }
+function liKlik(evt) {
+  const currentTarget = evt.currentTarget;
+  const target = evt.target;
+  console.log("currentTarget", currentTarget);
+  console.log("target", target);
+  const clickedId = currentTarget.dataset.id;
+  console.log(clickedId);
 
+  if (target.type === "checkbox") {
+    evt.preventDefault();
+    console.log("checkbox is clicked");
+
+    //  find taskobjektet i arrayet  ved hjælp af id
+    const idPlacering = list.findIndex((element) => element.id === clickedId);
+    console.log(idPlacering);
+    // gør noget ved objektet // arrayet
+
+    // update view
+  }
+  console.log("**************************");
+}
 //View
 function createClick() {
   addTaskToList({
